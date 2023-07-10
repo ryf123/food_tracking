184653a0313e0ffbaadaae4606a8d15e12ca7b89
Yifei Ren <ryf123@gmail.com>
2023-07-10 15:13:15 -0700

    Connect to OpenAI API

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -1,5 +1,8 @@
 import http.server
 import socketserver
+import os
+import openai
+import json
 
 # Define the port number for the server to listen on
 PORT = 8000
@@ -19,9 +22,36 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
             self.end_headers()
             with open('homepage.html', 'r') as f:
                 self.wfile.write(bytes(f.read(), encoding='utf-8'))
+        elif self.path == "/transcribe":
+            openai.api_key = os.getenv("OPENAI_API_KEY")
+            text = self.transcribe()
+            print(text)
+            calories = self.completion(text)
+            print(calories)
+            self.wfile.write(bytes(json.dumps(calories), 'utf-8'))
         else:
-            # Serve other files as usual
             super().do_GET()
+
+    def transcribe(self):
+        try:
+            audio_file= open("/Users/yifei/Downloads/recorded_audio.webm", "rb")
+            transcript = openai.Audio.transcribe("whisper-1", audio_file)
+            return transcript.text
+        except Exception as e:
+            print("An error occurred:", str(e))
+
+    def completion(self, prompt):
+      try:
+        response = openai.Completion.create(
+          model="text-davinci-003",
+          prompt=prompt,
+          max_tokens=200
+        )
+        print(response)
+        completion = response.choices[0].text.strip()
+        return completion
+      except Exception as e:
+        print("An error occurred:", str(e))
 
 # Create an instance of the HTTP server with the defined port and request handler
 with socketserver.TCPServer(("", PORT), SimpleHandler) as httpd:
