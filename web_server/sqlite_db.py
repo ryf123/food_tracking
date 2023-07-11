a3918f069ca97e5552949ef959304a3c6a7d6bc6
Yifei Ren <ryf123@gmail.com>
2023-07-11 14:14:47 -0700

    Add class to access sqlite database

---
+++ web_server/sqlite_db.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+class SQLiteDB:
+    TABLE_NAME = 'food_consumed'
+
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
+        if(not self.table_exists(self.TABLE_NAME)):
+            self.create_table()
+    
+    def table_exists(self, table_name):
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table_name,))
+        return self.cursor.fetchone() is not None
+    
+    def create_table(self):
+        self.cursor.execute(f'''
+            CREATE TABLE {self.TABLE_NAME} (
+                id INTEGER PRIMARY KEY,
+                user_id INTEGER,
+                transcribed_text TEXT,
+                translated_text TEXT,
+                calories INTEGER,
+                food_table BLOB,
+                timestamp REAL,
+                date TEXT,
+                time TEXT
+            )
+        ''')
+        self.conn.commit()
+
+    def insert(self, user_id, transcribed_text, translated_text, calories, food_table, timestamp, date, time):
+        self.cursor.execute(f"INSERT INTO {self.TABLE_NAME} (user_id, transcribed_text, translated_text, calories, food_table, timestamp, date, time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (user_id, transcribed_text, translated_text, calories, food_table, timestamp, date, time))
+        self.conn.commit()
+
+    def get_items(self):
+        self.cursor.execute(f"SELECT * FROM {self.TABLE_NAME}")
+        return self.cursor.fetchall()
+
+    def close(self):
+        self.conn.close()
+
