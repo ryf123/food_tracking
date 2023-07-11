8780756e8332936c193d640c695fa705be31dfb1
Yifei Ren <ryf123@gmail.com>
2023-07-11 16:50:13 -0700

    Refactor: move transcribe logic to a separate file

--- web_server/transcribe_service.py
+++ web_server/transcribe_service.py
@@ -1,41 +1,39 @@
-from sqlite_db import SQLiteDB
-import ast
-import datetime
-
+import os
+import openai
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import SequentialChain
 class TranscribeService:
-    def __init__(self):
-        self.db = SQLiteDB('mydatabase.db')
+	def __init__(self):
+		openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    def transcribe_and_insert(self, user_id):
-        # Perform transcription using OpenAI Whisper API
-        result = self.process_transcribed_result(self.transcribe_audio())
+	def summarize_calorie_intake(self, text):
+		llm = OpenAI(temperature=0)
+		translate_template = """Translate the text to English: {text} """
+		translate_prompt_template = PromptTemplate(input_variables=["text"], template=translate_template)
+		translate_chain = LLMChain(llm=llm, prompt=translate_prompt_template, output_key="translated_text")
 
-        self.db.insert(
-            user_id = user_id, 
-            transcribed_text = result['text'], 
-            translated_text = result['translated_text'], 
-            calories = result['calories'], 
-            food_table = result['table'], 
-            timestamp = datetime.datetime.utcnow().timestamp(),
-            date = str(datetime.date.today()),
-            time = str(datetime.datetime.now().time()))
+		estimate_calorie_template = """Estimate the calorie for each item, and put them in a table with format, |food_name|amount|estimate calorie|: {translated_text} """
+		estimate_calorie_prompt_template = PromptTemplate(input_variables=["translated_text"], template=estimate_calorie_template)
+		estimate_calorie_chain = LLMChain(llm=llm, prompt=estimate_calorie_prompt_template, output_key="table")
 
-    def process_transcribed_result(self, result):
-        result = ast.literal_eval(result.replace('\n', ''))
-        # convert 1,360 kcal to 1360
-        result['calories'] = int(result['total_calorie'].lower().strip().replace('kcal','').replace(",", ""))
-        return result
+		total_calorie_chain_tempate = """Sum the estimated total calorie taken in the table: {table}"""
+		total_calorie_prompt_template = PromptTemplate(input_variables=["table"], template=total_calorie_chain_tempate)
+		total_calorie_chain = LLMChain(llm=llm, prompt=total_calorie_prompt_template, output_key="total_calorie")
 
-    def transcribe_audio(self):
-        # Transcribe the audio using OpenAI Whisper API
-        # Replace this with your actual transcription logic
-        return '''{'text': '今天早上我吃了一碗粥,一个粽子 中午吃了一碗米饭,一些红烧肉和一些蔬菜 晚上吃了一碗米饭,几块烤鸭 一些红烧肉和一些蔬菜 还吃了两个小橘子,两个小的无花果', 'translated_text': '\n\nThis morning I had a bowl of porridge and a zongzi. For lunch I had a bowl of rice, some braised pork and some vegetables. For dinner I had a bowl of rice, a few pieces of roast duck, some braised pork and some vegetables. I also had two small oranges and two small figs.', 'table': '\n\n|Food Name|Amount|Estimate Calorie|\n|---|---|---|\n|Porridge|1 bowl|200 kcal|\n|Zongzi|1|200 kcal|\n|Rice|2 bowls|400 kcal|\n|Braised Pork|2 servings|400 kcal|\n|Vegetables|2 servings|100 kcal|\n|Roast Duck|2 pieces|200 kcal|\n|Oranges|2 small|80 kcal|\n|Figs|2 small|80 kcal|', 'total_calorie': '\n\n1,360 kcal'}'''
+		overall_chain = SequentialChain(chains=[translate_chain, estimate_calorie_chain, total_calorie_chain], input_variables=["text"], output_variables=["translated_text", "table", "total_calorie"], verbose=True)
+		result = overall_chain({'text': text})
+		print(result)
+		return result
 
-
-# Example usage:
-if __name__ == '__main__':
-    transcribe_service = TranscribeService()
-
-    transcribed_text = transcribe_service.transcribe_and_insert(100)
-    print(transcribe_service.db.get_items())
-    transcribe_service.db.close()
+	def transcribe_audio(self):
+		# Transcribe the audio using OpenAI Whisper API
+		# Replace this with your actual transcription logic
+		try:
+			audio_file= open("/Users/yifei/Downloads/recorded_audio.webm", "rb")
+			transcript = openai.Audio.transcribe("whisper-1", audio_file, prompt="The input is related to food consumed on a specific day")
+			return transcript.text
+		except Exception as e:
+			print("An error occurred:", str(e))
+			return '''{'text': '今天早上我吃了一碗粥,一个粽子 中午吃了一碗米饭,一些红烧肉和一些蔬菜 晚上吃了一碗米饭,几块烤鸭 一些红烧肉和一些蔬菜 还吃了两个小橘子,两个小的无花果', 'translated_text': '\n\nThis morning I had a bowl of porridge and a zongzi. For lunch I had a bowl of rice, some braised pork and some vegetables. For dinner I had a bowl of rice, a few pieces of roast duck, some braised pork and some vegetables. I also had two small oranges and two small figs.', 'table': '\n\n|Food Name|Amount|Estimate Calorie|\n|---|---|---|\n|Porridge|1 bowl|200 kcal|\n|Zongzi|1|200 kcal|\n|Rice|2 bowls|400 kcal|\n|Braised Pork|2 servings|400 kcal|\n|Vegetables|2 servings|100 kcal|\n|Roast Duck|2 pieces|200 kcal|\n|Oranges|2 small|80 kcal|\n|Figs|2 small|80 kcal|', 'total_calorie': '\n\n1,360 kcal'}'''
--- web_server/web_server.py
+++ web_server/web_server.py
@@ -1,14 +1,8 @@
 import http.server
 import socketserver
-import os
-import openai
 import json
-from langchain.llms import OpenAI
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
-from langchain.chains import SequentialChain
-from sqlite_db import SQLiteDB
 from db_access import DBAccess
+from transcribe_service import TranscribeService
 # Define the port number for the server to listen on
 PORT = 8000
 
@@ -31,11 +25,10 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
                 self.wfile.write(bytes(f.read(), encoding='utf-8'))
         elif self.path == "/transcribe":
             self.send_response(200)
-            openai.api_key = os.getenv("OPENAI_API_KEY")
-
-            text = self.transcribe()
+            ts = TranscribeService()
+            text = ts.transcribe_audio()
             print(text)
-            calories = self.summarize_calorie_intake(text)
+            calories = ts.summarize_calorie_intake(text)
             print(calories)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
@@ -55,46 +48,6 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
 
         else:
             super().do_GET()
-
-    def transcribe(self):
-        try:
-            audio_file= open("/Users/yifei/Downloads/recorded_audio.webm", "rb")
-            transcript = openai.Audio.transcribe("whisper-1", audio_file, prompt="The input is related to food consumed on a specific day")
-            return transcript.text
-        except Exception as e:
-            print("An error occurred:", str(e))
-
-    def summarize_calorie_intake(self, text):
-        llm = OpenAI(temperature=0)
-        translate_template = """Translate the text to English: {text} """
-        translate_prompt_template = PromptTemplate(input_variables=["text"], template=translate_template)
-        translate_chain = LLMChain(llm=llm, prompt=translate_prompt_template, output_key="translated_text")
-
-        estimate_calorie_template = """Estimate the calorie for each item, and put them in a table with format, |food_name|amount|estimate calorie|: {translated_text} """
-        estimate_calorie_prompt_template = PromptTemplate(input_variables=["translated_text"], template=estimate_calorie_template)
-        estimate_calorie_chain = LLMChain(llm=llm, prompt=estimate_calorie_prompt_template, output_key="table")
-
-        total_calorie_chain_tempate = """Sum the estimated total calorie taken in the table: {table}"""
-        total_calorie_prompt_template = PromptTemplate(input_variables=["table"], template=total_calorie_chain_tempate)
-        total_calorie_chain = LLMChain(llm=llm, prompt=total_calorie_prompt_template, output_key="total_calorie")
-
-        overall_chain = SequentialChain(chains=[translate_chain, estimate_calorie_chain, total_calorie_chain], input_variables=["text"], output_variables=["translated_text", "table", "total_calorie"], verbose=True)
-        result = overall_chain({'text': text})
-        print(result)
-        return result
-
-    def completion(self, prompt):
-      try:
-        response = openai.Completion.create(
-          model="text-davinci-003",
-          prompt=prompt,
-          max_tokens=200
-        )
-        print(response)
-        completion = response.choices[0].text.strip()
-        return completion
-      except Exception as e:
-        print("An error occurred:", str(e))
 
 # Create an instance of the HTTP server with the defined port and request handler
 with socketserver.TCPServer(("", PORT), SimpleHandler) as httpd:
